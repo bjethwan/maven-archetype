a199ca9434816596fb40353c27917ad3c43c0c57
Bipin Jethwani <jethwani.bipin@gmail.com>
2016-05-14 14:22:08 +0530

    my favorite jersey-hibernate-webapp-maven-archetype

---
+++ jersey-webapp-archetype/src/main/resources/archetype-resources/src/main/java/JerseyAppSetup.java
@@ -0,0 +1,13 @@
+package ${packageName};
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+
+@ApplicationPath("rest")
+public class JerseyAppSetup extends ResourceConfig{
+	public JerseyAppSetup(){
+		packages("jersey.samples");
+	}
+}
